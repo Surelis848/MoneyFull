ecb41f391506177f4ca984e9ec25ddd191ecbce6
Elena <elena.eigelyte@gmail.com>
2022-01-29 19:53:02 +0000

    Fixed the links and make app.py work

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/get_landing_page')
+@app.route('/')
 def get_landing_page():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 # Main function for running the app
 
 if __name__ == "__main__":
     #app.secret_key = 'mysecret',
-    app.debug = os.environ.get('FLASK_DEBUG', True)
     app.run(port=7000)
     #app.run(
             #host=os.environ.get('0.0.0.0'),
