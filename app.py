159a7a6c2ed7bc2b0e6042b35f5ee4c5ed67b03a
Elena <elena.eigelyte@gmail.com>
2022-01-31 16:29:04 +0000

    Connect to MongoDB and test the connection

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-import os
-from flask import Flask, render_template
-
+from bson.objectid import ObjectId
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "moneyfull"
+app.config["MONGO_URI"] = "mongodb+srv://surelis:rootUser@myfirstcluster-1cswe.mongodb.net/moneyfull?retryWrites=true"
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def get_landing_page():
-    return render_template('index.html')
+    return render_template('index.html', Categories=mongo.db.Categories.find())
+
+@app.route('/add_test')
+def add_test():
+    mongo.db.Categories.insert_one({'title': "Rent"})
+    return jsonify(message="success")
 
 
 # Main function for running the app
 
 if __name__ == "__main__":
-    #app.secret_key = 'mysecret',
-    app.run(port=7000)
+    app.run(port=7000, host='0.0.0.0', debug=True)
     #app.run(
             #host=os.environ.get('0.0.0.0'),
             #port=int(os.environ.get(5000)),
