0ee27d8d35a1871a1a277092ba7386c6bd0b7734
Elena <elena.eigelyte@gmail.com>
2022-02-03 08:14:44 +0000

    adding templates to flask

--- app.py
+++ app.py
@@ -12,7 +12,43 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def get_landing_page():
-    return render_template('landing.html', Categories=mongo.db.Categories.find())
+    return render_template('landing.html')
+
+@app.route('/register')
+def get_register_page():
+    return render_template('register.html', Categories=mongo.db.Categories.find())
+
+@app.route('/login')
+def get_login_page():
+    return render_template('login.html')
+
+@app.route('/tracker')
+def get_tracker_page():
+    return render_template('tracker.html')
+
+@app.route('/budget')
+def get_budget_page():
+    return render_template('budget.html')
+
+@app.route('/trends')
+def get_trends_page():
+    return render_template('trends.html')
+
+@app.route('/targets')
+def get_targets_page():
+    return render_template('targets.html')
+
+@app.route('/settings')
+def get_settings_page():
+    return render_template('settings.html')
+
+@app.route('/help')
+def get_help_page():
+    return render_template('help.html')
+
+@app.route('/error')
+def get_error_page():
+    return render_template('error.html')
 
 #@app.route('/add_test')
 #def add_test():
